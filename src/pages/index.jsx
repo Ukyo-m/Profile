cea280a76937f1b6bd3f871acf023f08844806d9
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-09 12:18:05 +0900

    フォームの分割とアクセスビリティの対応

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import Layout from "../components/layout"
+import Form from "../components/form"
 
 export default function Home({ data }) {
   return (
@@ -118,31 +119,7 @@ export default function Home({ data }) {
           </div>
         </section>
 
-        <section id="section5" className="section section5 contact-form">
-          <div className="content">
-            <h2>問い合わせ</h2>
-            <form className="contact-form" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-              <input type="hidden" name="form-name" defaultValue="contact" />
-              <div className="contact-form--info">
-                <div className="contact-form--info__name info__name">
-                  <label htmlFor className="info__name--label">お名前</label>
-                  <input type="text" className="info__name--input" />
-                </div>
-                <div className="contact-form--info__mail info__mail">
-                  <label htmlFor className="info__mail--label">メールアドレス</label>
-                  <input type="email" className="info__mail--input" />
-                </div>
-              </div>
-              <div className="contact-form--body">
-                <label htmlFor className="contact-form--body__label">内容</label>
-                <textarea name id className="contact-form--body__input" defaultValue={""} />
-              </div>
-              <div className="contact-form--submit">
-                <button className="contact-form--submit__btn">送信</button>
-              </div>
-            </form>
-          </div>
-        </section>
+        <Form />
 
       </div>
     </Layout>
