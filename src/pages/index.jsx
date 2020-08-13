915de8d4831ff0e3f1e1f73093812b8ad6f920c7
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-13 18:30:22 +0900

    レスポンシブ対応とアクセスビリティの対応

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -159,17 +159,17 @@ export default ({ data }) => {
             <input type="hidden" name="form-name" defaultValue="contact" />
             <div className="contact-form--info">
               <div className="contact-form--info__name info__name">
-                <label htmlFor className="info__name--label">お名前</label>
-                <input type="text" className="info__name--input" />
+                <label htmlFor className="info__name--label">お名前<input type="text" className="info__name--input" /></label>
               </div>
               <div className="contact-form--info__mail info__mail">
-                <label htmlFor className="info__mail--label">メールアドレス</label>
-                <input type="email" className="info__mail--input" />
+                <label htmlFor className="info__mail--label">メールアドレス<input type="email" className="info__mail--input" />
+                </label>
               </div>
             </div>
             <div className="contact-form--body">
-              <label htmlFor className="contact-form--body__label">内容</label>
-              <textarea name id className="contact-form--body__input" defaultValue={""} />
+              <label htmlFor className="contact-form--body__label">内容
+          <textarea name id className="contact-form--body__input" defaultValue={""} />
+              </label>
             </div>
             <div className="contact-form--submit">
               <button className="contact-form--submit__btn">送信</button>
