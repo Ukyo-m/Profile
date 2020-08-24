a4cfcbd4cabc783a21b5d2f7b4bc019f859ca4df
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-24 20:13:03 +0900

    navコンポーネントの修正

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,14 +1,24 @@
 import React from "react"
-import scrollTo from 'gatsby-plugin-smoothscroll';
+import scrollTo from "gatsby-plugin-smoothscroll"
 
 export default () => {
   return (
     <nav id="pagination" className="pagination">
-      <a id="pagination1" onClick={() => scrollTo('#section1')}>section 1</a>
-      <a id="pagination2" onClick={() => scrollTo('#section2')}>section 2</a>
-      <a id="pagination3" onClick={() => scrollTo('#section3')}>section 3</a>
-      <a id="pagination4" onClick={() => scrollTo('#section4')}>section 4</a>
-      <a id="pagination5" onClick={() => scrollTo('#section5')}>section 5</a>
+      <button id="pagination1" onClick={() => scrollTo("#section1")}>
+        section 1
+      </button>
+      <button id="pagination1" onClick={() => scrollTo("#section2")}>
+        section 2
+      </button>
+      <button id="pagination1" onClick={() => scrollTo("#section3")}>
+        section 3
+      </button>
+      <button id="pagination1" onClick={() => scrollTo("#section4")}>
+        section 4
+      </button>
+      <button id="pagination1" onClick={() => scrollTo("#section5")}>
+        section 5
+      </button>
     </nav>
   )
 }
