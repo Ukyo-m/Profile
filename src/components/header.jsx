7e9e1c81a729a9c864a165bc620ea4fc9882073f
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-24 23:05:31 +0900

    ヘッダーのアドレス指定

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,35 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => {
   return (
     <header className="header">
       <ul className="header--list">
         <li className="header--list__item list__item--link">
-          <a href className="home list__item--link-item">Home</a>
+          <Link className="home list__item--link-item" to="/">
+            Home
+          </Link>
         </li>
         <li className="header--list__item list__item--link">
-          <a href className="blog list__item--link-item">Blog</a>
+          <Link className="blog list__item--link-item" to="/blog">
+            Blog
+          </Link>
         </li>
         <li className="header--list__item list__item--link">
-          <a href className="github list__item--link-item">GitHub</a>
+          <a
+            href="https://github.com/Ukyo-m/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github list__item--link-item"
+          >
+            GitHub
+          </a>
         </li>
-        <li className="header--list__item list__item--link">
-          <a href className="twitter list__item--link-item">Twitter</a>
-        </li>
+        {/* <li className="header--list__item list__item--link">
+          <a href className="twitter list__item--link-item">
+            Twitter
+          </a>
+        </li> */}
       </ul>
     </header>
   )
