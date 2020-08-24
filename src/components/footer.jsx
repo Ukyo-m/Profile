00798dcc0304b573da5f2dfe4573470ac7ed0217
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-24 22:54:58 +0900

    フッターの年修正

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 
 export default () => {
+  var date = new Date()
+  var year = date.getFullYear()
+  console.log(year)
+
   return (
     <footer>
-      <p>Copyright © 2020 <a href>mash!ko</a> All Rights Reserved.</p>
+      <p>
+        Copyright © {year} <a href>mash!ko</a> All Rights Reserved.
+      </p>
     </footer>
   )
 }
