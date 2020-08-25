05c2cbad73470dbd9b9d4b76add8da177e96cdab
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-25 15:21:13 +0900

    ブログのキャッチアイと記事の作成

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,16 +2,20 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-export default ({ location }) => {
+export default () => {
   return (
     <Layout>
-      <div className="notfound">
-        <h1>
-          ブログページはただいま工事中です。
-          <br />
-          今しばらくお待ちください。
-        </h1>
+      <div className="catcheye">
+        <figure>
+          <img src="/thumb.jpg" alt="キャッチアイ画像の説明" srcset="" />
+        </figure>
       </div>
+
+      <article className="content">
+        <div className="container">
+          <h1>記事のタイトル</h1>
+        </div>
+      </article>
     </Layout>
   )
 }
