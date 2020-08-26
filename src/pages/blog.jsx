cce7f4dc06256fe098dd9594cc78257acac7ffda
Ukyo Mashiko <mycolor.palette.bigstory@live.jp>
2020-08-26 09:26:05 +0900

    ブログパーツの配置

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -14,7 +14,36 @@ export default () => {
       <article className="content">
         <div className="container">
           <h1>記事のタイトル</h1>
+
+          <aside className="info">
+            <time datetime="XXXX-XX-XX">XXXX年XX月XX日</time>
+            <div className="cat">
+              <ul>
+                <li className="slug">カテゴリーA</li>
+                <li className="slug">カテゴリーB</li>
+              </ul>
+            </div>
+          </aside>
+
+          <div className="postbody">
+            <p>
+              記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。記事の本文テキスト。
+            </p>
+          </div>
         </div>
+
+        <ul className="postlink">
+          <li className="prev">
+            <a href="">
+              <span>前の記事</span>
+            </a>
+          </li>
+          <li className="next">
+            <a href="">
+              <span>次の記事</span>
+            </a>
+          </li>
+        </ul>
       </article>
     </Layout>
   )
